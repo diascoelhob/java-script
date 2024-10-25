6e395f886f3d103baaf54ab281b59e1bdaf9fe12
diascoelhob <diascoelhob@gmail.com>
2024-10-25 19:50:38 -0300

    25-10-24

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 16
+if (idade < 16) {
+    console.log('Não vota!')
+} else {
+    if (idade < 18) {
+        console.log('Voto não obrigatorio!')
+    } else {
+        console.log('Voto obrigatorio!')
+    }
+}
