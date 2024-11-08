673c9179234f2cbeb817e8dcbb2f851707e2bb0a
diascoelhob <diascoelhob@gmail.com>
2024-11-08 08:08:22 -0300

    7-11-24

--- aula14/ex14/scripts/contando.js
+++ aula14/ex14/scripts/contando.js
@@ -2,4 +2,14 @@ function contando() {
     var inicio = Number(document.querySelector('input#idinicio').value)
     var fim = Number(document.querySelector('input#idfim').value)
     var passo = Number(document.querySelector('input#idpasso').value)
+
+    resposta = inicio
+
+    while (inicio < fim) {
+        inicio += passo
+        resposta += ` ${inicio}` 
+    }
+
+    var resultado = document.querySelector('section#resultado')
+    resultado.innerHTML = resposta
 }
