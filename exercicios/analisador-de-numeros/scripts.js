fdcef9068ad3622282ac7cce18ca4a398da7fe1f
diascoelhob <diascoelhob@gmail.com>
2024-11-23 14:12:13 -0300

    curso_finalizado

--- exercicios/analisador-de-numeros/scripts.js
+++ exercicios/analisador-de-numeros/scripts.js
@@ -8,49 +8,58 @@ let media = 0
 
 function cadastrar() {
     let entrada = Number(document.querySelector('input#num').value)
+    if (entrada < 1 || entrada >= 99) {
+        window.alert(`${entrada} - Número invalido.`)
+    } else {
+        let saida = document.querySelector('select#saida')
 
-    let saida = document.querySelector('select#saida')
+        /*Criando novo elemento HTML*/
+        let novo = document.createElement('option')
+        novo.textContent = `Valor ${entrada} adicionado.`
+        saida.appendChild(novo)
 
-    /*Criando novo elemento HTML*/
-    let novo = document.createElement('option')
-    novo.textContent = `Valor ${entrada} adicionado.`
-    saida.appendChild(novo)
+        /*Adicionando um valor ao Array (Vetor) num*/
+        num.push(entrada)
 
-    /*Adicionando um valor ao Array (Vetor) num*/
-    num.push(entrada)
+        /*Maior Valor*/
+        if (num.length == 0) {
+            maior = entrada
+        } else if (maior < entrada) {
+            maior = entrada
+        }
 
-    /*Maior Valor*/
-    if (num.length == 0) {
-        maior = entrada
-    } else if (maior < entrada) {
-        maior = entrada
+        /*Menor Valor*/
+        if (num.length == 0) {
+            menor = entrada
+        } else if (menor > entrada)
+            menor = entrada
+
+        /*Somoatório*/
+        soma += entrada
     }
-
-    /*Menor Valor*/
-    if (num.length == 0) {
-        menor = entrada
-    } else if (menor > entrada)
-        menor = entrada
-
-    /*Somoatório*/
-    soma += entrada
 }
 
 function analisar() {
     qtd = num.length
     media = soma/qtd
 
-    Quantidade: ${qtd}
-    Maior valor: ${maior}
-    Menor valor: ${menor}
-    Somatório: ${soma}
-    Média: ${media}
-}
+    if (qtd == 0) {
+        window.alert('Adicione pelo menos um número.')
+    } else {
+        let resultado = document.querySelector('ul#resultado')
+        resultado.innerHTML = `
+        <li>Quantidade: ${qtd}</li>
+        <li>Maior valor: ${maior}</li>
+        <li>Menor valor: ${menor}</li>
+        <li>Somatório: ${soma}</li>
+        <li>Média: ${media}</li>`
 
-/*
-Quantidade de números
-O maior valor
-O menor valor
-Somatório de todos os valores
-A média
-*/
+        resultado.style.transform = 'translateY(-114.5px)'
+
+        let main = document.querySelector('main')
+        main.style.height = '283.500px'
+
+        let analisar = document.querySelector('button#analisar')
+        analisar.style.transform = 'translateY(-114.5px)'
+    }  
+}
